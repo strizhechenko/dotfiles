c1bd45302b9f0326cd992f6ea5c26f4fe7c4c6bb
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2018-01-05 21:14:07 +0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 get brew
-brew install git vim python2 python3 xquartz dot graphviz openvpn ruby gem clang clang-check clang-format shellcheck
+brew install git vim python2 python3 xquartz dot graphviz openvpn ruby gem clang clang-check clang-format shellcheck valgrind qcachegrind
 get pip2 pip3 shiftit thunderbird firefox noscript ublock oh-my-zsh pycharm clion phpstorm atom twitter telephone sourcetree docker docker-compose
 pip install ipython pytest yaml dictator pylint
 setup terminal (pro and font, no sound, no limit) ssh-keyring timemachine shiftit hotkeys cmd-space input switching jetbrains auth
