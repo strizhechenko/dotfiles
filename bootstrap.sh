0eb1ac2f49ac9b9b73901e373c12db83ced210d4
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2018-01-05 21:09:49 +0500

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+get brew
+brew install git vim python2 python3 xquartz dot graphviz openvpn ruby gem clang clang-check clang-format shellcheck
+get pip2 pip3 shiftit thunderbird firefox noscript ublock oh-my-zsh pycharm clion phpstorm atom twitter telephone sourcetree docker docker-compose
+pip install ipython pytest yaml dictator pylint
+setup terminal (pro and font, no sound, no limit) ssh-keyring timemachine shiftit hotkeys cmd-space input switching jetbrains auth
+gem install fpm
